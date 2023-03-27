8722bd99eaf80703b53d44433dab2d8ae6cd8736
tonthan <tonthan4452@gmail.com>
2023-03-27 13:18:53 +0700

    Update UserItem.js

--- my-product-app/src/UserItem.js
+++ my-product-app/src/UserItem.js
@@ -7,14 +7,20 @@ export default function UserItem(props) {
     }
 
     return (
-        <div className="row border rounded shadow-sm mt-3">
+        <>
+        <div className="row border rounded shadow-sm mt-5">
            
             <div className="col-7">
                 <h5 className="text-rpimary">{props.data.user_name}</h5>
                 <Link to={`/user/${props.data.user_id}`} className="btn btn-outline-primary me-3">แก้ไข</Link>
                 <button type="button" className="btn btn-outline-danger" onClick={onDeleteU}>ลบผู้ใช้</button>
             </div>
-            
+            <div className="col-3">
+                <span className="text-danger fs-9"> {props.data.user_type_name} </span>
+            </div>
+          
         </div>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+       </> 
     );
 }
